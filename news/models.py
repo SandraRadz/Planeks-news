4e402c2179b5d4ed53e0b3ebf9a60c5ac327e5b6
SandraRadz <oleksandraradzievska@gmail.com>
2020-05-27 20:59:32 +0300

    fix

--- news/models.py
+++ news/models.py
@@ -55,4 +55,4 @@ class Comment(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата написания")
 
     def __str__(self):
-        return "Комментарий "+self.author.username
+        return "Комментарий "+self.author.email
