19d525365b4e83521d8c557273ccc18b1bdaf8d0
SandraRadz <oleksandraradzievska@gmail.com>
2020-05-27 15:01:41 +0300

    edit permission

--- news/models.py
+++ news/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -6,6 +8,7 @@ class New(models.Model):
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
+        permissions = [('can_publish_new_without_approve', 'Публикация новостей без премодерации')]
 
     class NewsStatus(models.TextChoices):
         CREATED = 'C', 'создано'
@@ -18,6 +21,23 @@ class New(models.Model):
     status = models.CharField(max_length=1, verbose_name="Статус", choices=NewsStatus.choices, default=NewsStatus.CREATED)
     date_created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
     pub_date = models.DateTimeField(verbose_name='Дата публикации', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        """
+            * create
+                if user has permission can_publish_new_without_approve,
+                new will be approved and pub_date will be set on now immediately
+            * edit
+                after someone has edited new,
+                check whether value of status is equal to "approved" and
+                if this new hasn't been approved before set pub_date value on now
+        """
+        if self.pk is None and self.author.has_perm('news.can_publish_new_without_approve'):
+            self.status = "A"
+            self.pub_date = datetime.now()
+        if self.status == 'A' and self.pub_date is None:
+            self.pub_date = datetime.now()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
